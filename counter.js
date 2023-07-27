35d0dd944407ef31bb177b1ab40fabe9a4ba5621
Mbiatke Nkanta <mbiatke.nkanta@gmail.com>
2023-07-27 07:18:33 +0100

    counter application

---
+++ counter.js
@@ -0,0 +1,66 @@
+const count = document.getElementById('count');
+let counter = 0
+
+function increaseBtn(){
+    counter = counter + 1;
+    count.innerHTML = counter
+}
+function decreaseBtn(){
+counter = counter - 1;
+count.innerHTML = counter
+}
+
+function doublex2(){
+    counter = counter * 2;
+    count.innerHTML = counter
+}
+function reset(){
+    counter = counter = 0;
+    count.innerHTML = counter 
+}
+function square(){
+    counter = counter * counter;
+    count.innerHTML = counter
+}
+function x5(){
+    counter = counter * 5;
+    count.innerHTML = counter
+}
+function cube(){
+    counter = counter * counter * counter;
+    count.innerHTML = counter 
+}
+
+function squareroot(){
+    counter = counter * counter * 3;
+    count.innerHTML = counter
+}
+function x3(){
+    counter = counter * 3;
+    count.innerHTML = counter
+}
+function x25(){
+    counter = counter * 25;
+    count.innerHTML = counter
+}
+function x7(){
+    counter = counter * 7;
+    count.innerHTML = counter
+}
+function x9(){
+    counter = counter * 9;
+    count.innerHTML = counter
+}
+function x12(){
+    counter = counter * 12;
+    count.innerHTML = counter
+}function x50(){
+    counter = counter * 50;
+    count.innerHTML = counter
+}
+function plus4(){
+    counter = counter + 4;
+    count.innerHTML = counter
+}
+
+
---
+++ index.js
@@ -0,0 +1,42 @@
+ // const Name = "Galvin"
+// const age = 54
+// const boolean = true
+
+// console.log(Name);
+// console.log(54);
+// console.log(true);
+
+// const base = 15
+// const height = 20 
+
+// const area = height * base /2
+
+// console.log(area);
+
+// const radius = 5;
+// const area = 3.14 * 25
+
+// console.log(area);
+
+// const Base = 25
+// const height = 30
+// const base = 40
+
+// const area = 65 * 30/2
+
+// console.log(area);
+
+// Math.sqrt()
+
+// const side = 15;
+// const sqrt1 = Math.sqrt(5);
+// const sqrt2 = Math.sqrt(5 + 2 * sqrt1);
+// const area = 5/2 * side * side * sqrt2
+// console.log(area);
+
+// const side = 25;
+// const sqrt3 = Math.sqrt(5);
+// const sqrt4 = Math.sqrt(5 * 7 * sqrt3)
+// const area = 1/4 * sqrt4 * side * side;
+
+// console.log(area);
